6eb5b51c3bbd54abe4ce1469a2ea110337f1ccdc
Zepe <info@zepe.com>
2026-05-09 07:03:10 +0000

    feat: add planilla de ventas y egresos doctypes, workspace and chart

---
+++ planillaventasyegresos/planilla_de_ventas_y_egresos/doctype/planilla_de_ventas_y_egresos/planilla_de_ventas_y_egresos.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Zepe and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Planilla de Ventas y Egresos", {
+// 	refresh(frm) {
+
+// 	},
+// });
